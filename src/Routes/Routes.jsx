4a25c70d904e556a7ab6ea42b0088a65d0bd38fe
Sadman Sakib <sadmansakib123450@gmail.com>
2023-05-12 23:07:46 +0200

    firebase auth added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Home/Login/Login";
+import SignUp from "../pages/Home/Signup/Signup";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        },
+        {
+          path:'/login',
+          element:<Login></Login>
+        },
+        {
+          path:'/signup',
+          element:<SignUp></SignUp>
+        }
+      ]
+    },
+  ]);
+
+  export default router
