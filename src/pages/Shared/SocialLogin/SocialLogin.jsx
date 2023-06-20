9aebf29569eeaa949758d45ab0d5f207228077df
Sadman Sakib <sadmansakib123450@gmail.com>
2023-06-21 00:46:16 +0200

    add UseLocation and Navigation for smoothe login redirect

--- src/pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/pages/Shared/SocialLogin/SocialLogin.jsx
@@ -1,12 +1,17 @@
 import { useContext } from "react";
 import { AuthContext } from "../../../providers/AuthProvider";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || '/';
     const { googleSignIn } = useContext(AuthContext);
     const handleGoogleSignIn = () => {
         googleSignIn()
             .then(result => {
                 console.log(result.user);
+                navigate(from, { replace: true })
             })
             .catch(error => console.log(error))
     }
